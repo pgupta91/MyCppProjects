bb1b8f12cdbe98b9ab847b8865f8e8d78e03e99c
pgupta91 <pgupta91sdsu@gmail.com>
2017-06-27 19:44:45 -0700

    Adding problem discription

--- Source.cpp
+++ Source.cpp
@@ -1,3 +1,15 @@
+/*In the programming language of your choice, write a program generating the first n Fibonacci numbers F(n), printing
+
+"Buzz" when F(n) is divisible by 3.
+"Fizz" when F(n) is divisible by 5.
+"FizzBuzz" when F(n) is divisible by 15.
+"BuzzFizz" when F(n) is prime.
+the value F(n) otherwise.
+We encourage you to compose your code for this question in a way that represents the quality of code you produce in the workplace - e.g. tests, documentation, linting, dependency management (though there's no need to go this far).
+
+Please upload your code to GitHub or bitbucket and post a link to the repo or gist as your answer in the Lever form.
+*/
+
 #include <iostream>
 #include <string>
 using namespace std;
